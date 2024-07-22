053e457f6d0c9dc1f7f6b34c04bc08cdc632749b
dwalczak97 <d.melon@op.pl>
2024-07-22 14:29:25 +0200

    jebać

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,2 @@
-// const liItem = document.querySelectorAll(".item");
-
-// liItem.forEach((categories) => {
-//   `Number of categories:` liItem.length;
-// });
-
-// // const liItem = document.querySelectorAll(".item");
-
-// const superFun = () => {
-//   const allItem = document.querySelectorAll("ul > li");
-//   const x = allItem.length;
-//   console.log(`Number of categories: ${x}`);
-// };
+const everyLi = document.querySelectorAll("ul#categories > li");
+console.log(everyLi);
