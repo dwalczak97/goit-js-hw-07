9f518e9b170e301da960e0e777783409ce5e03de
dwalczak97 <d.melon@op.pl>
2024-07-23 16:41:15 +0200

    on

--- js/task-1.js
+++ js/task-1.js
@@ -1,2 +1,8 @@
-const everyLi = document.querySelectorAll("ul#categories > li");
-console.log(everyLi);
+const allItems = document.querySelectorAll(".item");
+console.log(`Number of categories:`, allItems.length);
+allItems.forEach((category) => {
+  const itemName = category.querySelector("h2").textContent;
+  const smallListOne = category.querySelectorAll("li").length;
+  console.log(`Category:`, itemName);
+  console.log(`Elements:`, smallListOne);
+});
